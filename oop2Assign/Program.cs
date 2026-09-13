cd74b261604a02ac9171f331b0ce94e267f7c7d7
yousif <yshehab693@gmail.com>
2026-09-13 13:35:18 +0300

    part 1 Q1 b) Why are classes more suitable than structs for large applications?

--- oop2Assign/Program.cs
+++ oop2Assign/Program.cs
@@ -5,8 +5,14 @@
         static void Main(string[] args)
         {
             #region part 1 Q1 a) What is the difference between a class and a struct?
-              // struct is a value type used for leight weigth data and stored in heap and does not support inheritance 
-              // class is a reference type stored in heap and it is the main oop unit because it supports all the 4 pillars of oop
+            // struct is a value type used for leight weigth data and stored in heap and does not support inheritance 
+            // class is a reference type stored in heap and it is the main oop unit because it supports all the 4 pillars of oop
+            #endregion
+
+            #region part 1 Q1 b) Why are classes more suitable than structs for large applications?
+             // Because class support inheritence which helps in reusability and organization of code in large projects
+             // classes are refrence type so objects can be shared between diffrent parts of the App without copying the entire projects
+             // Better for complex projects and reusable oop designs
             #endregion
         }
     }
