c1aa9fa50c893e3c5088331bb616dbd63f473fc0
yousif <yshehab693@gmail.com>
2026-09-13 13:30:18 +0300

    part1 Q1 a) Diff between struct and class

--- oop2Assign/Program.cs
+++ oop2Assign/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-
+            #region part 1 Q1 a) What is the difference between a class and a struct?
+              // struct is a value type used for leight weigth data and stored in heap and does not support inheritance 
+              // class is a reference type stored in heap and it is the main oop unit because it supports all the 4 pillars of oop
+            #endregion
         }
     }
 }
