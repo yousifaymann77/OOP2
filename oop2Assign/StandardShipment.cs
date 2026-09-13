a6099edbdae35e77fac351fc073aa698027ad241
yousif <yshehab693@gmail.com>
2026-09-13 14:12:01 +0300

    part 2 Q2 Part (1)  standardShipment with ctor chaining

---
+++ oop2Assign/StandardShipment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop2Assign
+{
+    internal class StandardShipment : Shipment
+    {
+        #region part 2 Q2 standardShipment with ctor chaining
+        public StandardShipment(string trackingCode, string description, decimal deliveryFee, decimal weight, string destinantio)
+            : base(trackingCode, description, deliveryFee, weight, destinantio)
+        {
+        } 
+        #endregion
+    }
+}
